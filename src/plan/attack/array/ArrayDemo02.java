4d892ca7156db666ea00276293d42c752af0e4d6
沈张一 <3256152226@qq.com>
2026-04-23 09:39:48 +0800

    第三章_03静态数组的不同格式的存储

---
+++ src/plan/attack/array/ArrayDemo02.java
@@ -0,0 +1,15 @@
+package plan.attack.array;
+
+public class ArrayDemo02 {
+    public static void main(String[] args) {
+        //本次实验旨在测试常规数组元素访问
+        //以及尝试进行迭代器访问
+        int[] arr={1,2,3,4,5,6};
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+        for (int v : arr) {
+            System.out.println(v);
+        }
+    }
+}
